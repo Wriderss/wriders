5a6d8d3991a4c6e7a1abdaa5d03069cf5b598ce2
Aditya Painuli <adityapainuli2004@gmail.com>
2022-09-27 09:16:50 +0530

    refactoring code

--- pages/notification-page.tsx
+++ pages/notification-page.tsx
@@ -75,10 +75,10 @@ const NotificationPage: NextPage = () => {
           profilePhoto={userDetails?.profilePhoto}
         />
         <div className="w-[90%] flex flex-col space-y-4  mx-auto my-4 p-2 ">
-          {userUpdates.map((updates: any, index: number) => (
+          {userUpdates?.map((updates: any, index: number) => (
             <div
               key={index}
-              onClick={() => router.push(`/${updates.slug}`)}
+              onClick={() => router.push(`/${updates?.slug}`)}
               className={`h-auto cursor-pointer flex space-x-4  items-center p-2 w-full hover:bg-gray-200 rounded-md ${
                 mode
                   ? "bg-gray-800 text-white hover:bg-gray-700"
@@ -88,57 +88,70 @@ const NotificationPage: NextPage = () => {
               <div className="relative h-[200px] w-[250px] ">
                 <Image
                   priority
-                  src={updates.image}
-                  alt={updates.title}
+                  src={updates?.image}
+                  alt={updates?.title}
                   layout="fill"
                   className="object-contain"
                 />
               </div>
               <div className=" flex-1 ">
-                <h1 className="font-semibold text-xl">{updates.title}</h1>
+                <h1 className="font-semibold text-xl">{updates?.title}</h1>
                 <div className="flex items-center space-x-2">
                   <div className="flex  items-center">
                     <span className="font-semibold">
-                      {updates.likes.length}
+                      {updates?.likes?.length}
                     </span>
                     <HeartIcon height={30} width={30} color="red" />
                   </div>
-                  <div className="flex items-center flex-1 space-x-2">
-                    {updates.likes.map(({ user, userId }: any) => (
-                      <Image
-                        src={user.profilePhoto}
-                        alt={user.name}
-                        height={30}
-                        width={30}
-                        className="rounded-full cursor-pointer p-[2rem]"
-                      />
-                    ))}
-                  </div>
-                </div>
-                <div className="my-2">
-                  <h2 className="font-semibold ">Comments</h2>
-                  {updates.comment.map(({ author, body }: any) => (
-                    <div key={author.id} className="flex flex-col my-2 ">
-                      <div className="flex items-center space-x-1">
-                        <Image
-                          src={
-                            author.profilePhoto
-                              ? author.profilePhoto
-                              : `https://avatars.dicebear.com/api/avataaars/${author.email}.svg`
-                          }
-                          alt={author.name}
-                          height={30}
-                          width={30}
-                          className="rounded-full border border-gray-500"
-                        />
-                        <span className="text-[13px] ">{author.name}</span>
-                      </div>
-                      <div className="ml-8">
-                        <p className="text-[15px]">{body}</p>
-                      </div>
+                  {userUpdates && (
+                    <div className="flex items-center flex-1 space-x-2">
+                      {updates?.likes.map(
+                        ({ user, userId }: any, index: number) => (
+                          <Image
+                            key={index}
+                            src={
+                              user?.profilePhoto
+                                ? user.profilePhoto
+                                : "/user.png"
+                            }
+                            alt={user?.name}
+                            height={30}
+                            width={30}
+                            className="rounded-full cursor-pointer p-[2rem]"
+                          />
+                        )
+                      )}
                     </div>
-                  ))}
+                  )}
                 </div>
+                {userUpdates && (
+                  <div className="my-2">
+                    <h2 className="font-semibold ">Comments</h2>
+                    {updates.comment.map(
+                      ({ author, body }: any, index: number) => (
+                        <div key={index} className="flex flex-col my-2 ">
+                          <div className="flex items-center space-x-1">
+                            <Image
+                              src={
+                                author?.profilePhoto
+                                  ? author?.profilePhoto
+                                  : `https://avatars.dicebear.com/api/avataaars/${author.email}.svg`
+                              }
+                              alt={author?.name}
+                              height={30}
+                              width={30}
+                              className="rounded-full border border-gray-500"
+                            />
+                            <span className="text-[13px] ">{author?.name}</span>
+                          </div>
+                          <div className="ml-8">
+                            <p className="text-[15px]">{body}</p>
+                          </div>
+                        </div>
+                      )
+                    )}
+                  </div>
+                )}
               </div>
             </div>
           ))}
